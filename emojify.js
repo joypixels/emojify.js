49823a29c9be40787b0196af7673ebe9fbccdb02
Peter Hizalev <peter.hizalev@gmail.com>
2014-04-09 20:12:31 +0000

    Support emoticons_enabled config

--- emojify.js
+++ emojify.js
@@ -79,7 +79,8 @@
                     'A': 1,
                     'PRE': 1,
                     'CODE': 1
-                }
+                },
+                emoticons_enabled: true
             };
 
             /* Returns true if the given char is whitespace */
@@ -108,9 +109,11 @@
                     if(namedMatchHash[named]) { return named; }
                     return;
                 }
-                for(var i = 3; i < match.length - 1; i++) {
-                    if(match[i]) {
-                        return emoticonsProcessed[i - 2][1];
+                if (defaultConfig.emoticons_enabled) {
+                    for(var i = 3; i < match.length - 1; i++) {
+                        if(match[i]) {
+                            return emoticonsProcessed[i - 2][1];
+                        }
                     }
                 }
             }
