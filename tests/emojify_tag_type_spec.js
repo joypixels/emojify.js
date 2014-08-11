3d112353646655410067a1a923cb43c6751c7064
Sean DuBois <sean@siobud.com>
2014-08-11 14:10:49 -0500

    Update emojify_tag_type tests from review by @qq99 on PR #40, tests now use both codepaths .replace and .run

--- tests/emojify_tag_type_spec.js
+++ tests/emojify_tag_type_spec.js
@@ -1,7 +1,7 @@
 JS.Test.describe('emojify with differing tag_types', function() {
     'use strict';
 
-    this.describe('emojify creation of img elements', function() {
+    this.describe('emojify using .replace', function() {
         this.it('should default to img element', function() {
             var text = ':)';
             var result = emojify.replace(text);
@@ -16,9 +16,7 @@ JS.Test.describe('emojify with differing tag_types', function() {
             var result = emojify.replace(text);
             this.assertEqual('<img title=\':blush:\' alt=\':blush:\' class=\'emoji\' src=\'images/emoji/blush.png\' align=\'absmiddle\' />', result);
         });
-    });
 
-    this.describe('emojify creation of user specifed tag type via emojify_tag_type', function() {
         this.it('div emojify_tag_type should be well formed', function() {
             emojify.setConfig({
                 emojify_tag_type: 'div'
@@ -29,6 +27,29 @@ JS.Test.describe('emojify with differing tag_types', function() {
         });
     });
 
+    this.describe('emojify using .run', function() {
 
+        this.before(function() {
+            this.el = document.createElement("DIV");
+        });
 
+        this.it('null emojify_tag_type should be img element', function() {
+            emojify.setConfig({
+                emojify_tag_type: null
+            });
+            this.el.innerHTML = ":)";
+            emojify.run(this.el);
+            this.assertEqual('<img class="emoji" src="images/emoji/blush.png" title=":blush:" alt=":blush:" align="absmiddle"></img>', this.el.innerHTML);
+        });
+
+        this.it('div emojify_tag_type should be well formed', function() {
+            emojify.setConfig({
+                emojify_tag_type: 'div'
+            });
+            this.el.innerHTML = ":)";
+            emojify.run(this.el);
+            this.assertEqual('<div class="emoji emoji-blush" title=":blush:" alt=":blush:" align="absmiddle"></div>', this.el.innerHTML);
+        });
+
+    });
 });
