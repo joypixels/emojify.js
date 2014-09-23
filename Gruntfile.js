9b9b3c30201f134a405e702f5e003aa265e7bfb3
Adam Lynch <contact@adamlynch.ie>
2014-09-23 10:40:43 +0100

    Merge in from master

--- Gruntfile.js
+++ Gruntfile.js
@@ -54,6 +54,7 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-mocha-test');
     grunt.loadNpmTasks('grunt-datauri');
 
     grunt.registerTask('test-node', 'mochaTest');
