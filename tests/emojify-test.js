cc2ae552998dfc7bdbde308932d8d0898b563de4
Andy Trevorah <andy@troupe.co>
2014-10-28 22:59:30 +0000

    added emojify function that uses emojify-parser

---
+++ tests/emojify-test.js
@@ -0,0 +1,18 @@
+var emojify = require('../lib/emojify');
+var jsdom = require('jsdom');
+var assert = require('assert');
+
+describe('emojify', function() {
+    it('emojifies an element', function(done) {
+        jsdom.env('<span id="cat">hello :cat:</span>', function(err, window) {
+            if(err) return done(err);
+
+            var el = window.document.getElementById('cat');
+
+            emojify(el, { document: window.document });
+
+            assert.equal(el.outerHTML, '<span id="cat">hello <img class="emoji" src="/cat.png" title=":cat:" alt=":cat:" align="absmiddle"></span>');
+            done();
+        });
+    });
+});
