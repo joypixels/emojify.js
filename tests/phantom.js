29a62b4a50e2d23545eec2a51e5d60a40c1117c9
Adam Lynch <contact@adamlynch.ie>
2014-11-20 12:13:08 +0000

    CI: Removed phantomjs debugging

--- tests/phantom.js
+++ tests/phantom.js
@@ -1,6 +1,5 @@
 /* global JS:false, phantom:false */
 
-throw new Error('debugging phantomjs');
 phantom.injectJs('./node_modules/jstest/jstest.js');
 var options  = {format: 'dot'},
     reporter = new JS.Test.Reporters.Headless(options);
