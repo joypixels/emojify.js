5e24b65d1feed2c1cdf8382f5ea7e6ae9e75938b
Adam Lynch <contact@adamlynch.ie>
2014-11-20 12:09:01 +0000

    CI: Debugging phantomjs

--- tests/phantom.js
+++ tests/phantom.js
@@ -1,5 +1,6 @@
 /* global JS:false, phantom:false */
 
+throw new Error('debugging phantomjs');
 phantom.injectJs('./node_modules/jstest/jstest.js');
 var options  = {format: 'dot'},
     reporter = new JS.Test.Reporters.Headless(options);
